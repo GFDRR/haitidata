4c7aaaa295a05398aaaa6cde50f164c97c6d7287
Ariel Nunez <ingenieroariel@gmail.com>
2011-04-25 21:46:59 -0400

    Added basic setup.py and code folder with __init__.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+import os
+from setuptools import setup, find_packages
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+setup(
+    name="haitidata",
+    version="0.1",
+    author="Ariel Nunez, Jeffrey Johnson",
+    author_email="ingenieroariel@gmail.com",
+    description="Code behind haitidata.org",
+    long_description=(read('README')),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Framework :: GeoNode',
+        'License :: OSI Approved :: GNU Library or General Public License (GPL)',
+    ],
+    license="GPL 3",
+    keywords="haitidata geonode django",
+    url='https://github.com/GFDRR-Labs/haitidata',
+    packages=find_packages('.'),
+    include_package_data=True,
+    zip_safe=False,
+)
