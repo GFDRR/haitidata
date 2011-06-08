53d38191cd5cf1b14a1f5c801b85c4a8e322d981
Ariel Nunez <ingenieroariel@gmail.com>
2011-06-08 07:32:17 -0700

    Added logging by default

--- haitidata/settings.py
+++ haitidata/settings.py
@@ -282,7 +282,6 @@ LOGGING = {
         'mail_admins': {
             'level': 'ERROR',
             'class': 'django.utils.log.AdminEmailHandler',
-            'filters': ['special']
         }
     },
     'loggers': {
